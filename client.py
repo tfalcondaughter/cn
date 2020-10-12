19cb62600568b71bdc258a96654a09a7b54ed4b2
Tetiana Sokolova <tanya.falcondaughter@gmail.com>
2020-10-12 23:46:23 +0300

    Почала робити під свій варіант, взаємодія клієнт-сервер

--- client.py
+++ client.py
@@ -1,8 +1,24 @@
 import socket as s
 
+# Контейнер іменованих об'єктів.
+# На сервері зберігаються іменовані об'єкти клієнтів: цілі числа, символьні рядки тощо. Для зберігання/зміни на сервері
+# об'єкта потрібно передати трійку <Ім'я; Тип; Значення>. Для зчитування потрібне лише ім'я, а сервер має дати відповідь
+# трійкою <Ім'я; Тип; Значення>. Клієнт може зчитати, змінити та отримати перелік імен об'єктів.
+
 S = s.socket(s.AF_INET, s.SOCK_STREAM)
 host = s.gethostname()
 port = 1111
 S.connect((host, port))
-print("Connected to server on:%s" % S.recv(1024).decode('ascii'))
+print("Connected to server on:", S.recv(1024).decode('ascii'))
+
+command = input("Enter a command: ")
+S.send(command.encode('ascii'))
+if command == 'PRINT':
+    text = input("Which object: ")
+    S.send(text.encode('ascii'))
+    a = S.recv(1024)
+    b = S.recv(1024)
+    c = S.recv(1024)
+    print(a.decode('ascii'), ';', b.decode('ascii'), ';', c.decode('ascii'))
+
 S.close()
--- server.py
+++ server.py
@@ -5,11 +5,30 @@ server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 host = s.gethostname()
 port = 1111
 server_socket.bind((host, port))
-server_socket.listen()
+server_socket.listen(5)
 
-if True:
+my_set = {('cat_weight', 'int', 5), ('dog_weight', 'int', 10), ('elephant_weight', 'int', 500)}
+
+
+def commands(server_set, client_text, command):
+    if command == 'PRINT':
+        for set_list in server_set:
+            if client_text == set_list[0]:
+                client_socket.send(set_list[0].encode('ascii'))
+                client_socket.send(set_list[1].encode('ascii'))
+                client_socket.send(str(set_list[2]).encode('ascii'))
+    elif command == 'GET_OBJECTS_NAMES':
+        for set_list in server_set:
+            print(set_list[0])
+
+
+e = b'no'
+while e.decode('ascii') != 'exit':
     client_socket, address = server_socket.accept()
-    print("Connected with[address],[port]%s" % str(address))
-    currentTime = t.ctime(t.time()) + "\r\n"
+    print("Connected with:", address[0], "address:", address[1])
+    currentTime = t.ctime(t.time()) + '\n'
     client_socket.send(currentTime.encode('ascii'))
+    e = client_socket.recv(1024)
+    text = client_socket.recv(1024)
+    commands(my_set, text.decode('ascii'), e.decode('ascii'))
     client_socket.close()
